0730298ad3b59b516f652335c2a66f70aee2d9ef
MishynPavel <68873173+MishynPavel@users.noreply.github.com>
2020-12-23 18:08:39 +0300

    Create script.js

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// Показываем окно
+function viewDetail(){
+  document.getElementById("detail").style.display = "block";
+};
+// Скрываем окно
+function hideDetail(){
+  document.getElementById("detail").style.display = "none";
+};
